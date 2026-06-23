8ad48ce1b0597cafdb3dc810c7aedfa3736cc651
DataKinds <you@example.com>
2026-06-23 01:50:00 -0700

    Most of evaluator

--- src/Main.purs
+++ src/Main.purs
@@ -5,19 +5,25 @@ import Data.Maybe
 import Prelude
 
 import App.Button as Button
-import Control.Monad.Error.Class (class MonadThrow)
+import Control.Monad.Error.Class (class MonadThrow, catchError, throwError)
 import Control.Monad.Except (ExceptT)
 import Data.Array ((:))
 import Data.Array as A
+import Data.Array.NonEmpty as NA
 import Data.HashMap (HashMap)
 import Data.HashMap as HM
 import Data.List as L
+import Data.List.Lazy as LL
+import Data.Newtype (class Newtype, unwrap)
 import Data.String as S
+import Data.Traversable (sequence, sequence_)
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
-import Effect.Class (class MonadEffect)
+import Effect.Class (class MonadEffect, liftEffect)
+import Effect.Exception (Error)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
+import Web.DOM.Document (doctype)
 
 type RValue = String -- Runtime value
 type RStack = Array RValue
@@ -30,19 +36,27 @@ data Definition evalM = Native (evalM Unit) | Canon (Array String)
 type Module evalM =
   { chain :: Array ModuleName
   , defs :: HashMap WordName (Definition evalM)
-  , stacks :: HashMap StackName RStack }
+  , stacks :: HashMap StackName RStack
+  }
+
 emptyModule :: forall m. Module m
 emptyModule = { chain: mempty, defs: HM.empty, stacks: mempty }
+
 emptyStack :: RStack
 emptyStack = mempty
-newtype RealState = RealState 
+
+newtype RealState = RealState
   { modules :: HashMap ModuleName (Module RealEval)
-  , openModule :: ModuleName
+  , openModules :: NA.NonEmptyArray ModuleName
   , source :: Array String
-  , sourceIx :: Int }
--- emptyRealState :: RealState 
--- emptyRealState = RealState { modules: a, openModule:a, source:a, sourceIx:a }
-type RealEval = StateT RealState (ExceptT String Effect)
+  , sourceIx :: Int
+  }
+derive instance newtypeRealState :: Newtype RealState _
+
+emptyRealState :: RealState
+emptyRealState = RealState { modules: HM.empty, openModules: NA.singleton "main", source: [], sourceIx: 0 }
+
+type RealEval = StateT RealState (ExceptT Error Effect) -- TODO: custom Error datatype, with MonadThrow/MonadCatch OurError Effect instances
 
 getOrMakeModule :: ModuleName -> RealEval (Module RealEval)
 getOrMakeModule mn = do
@@ -53,23 +67,24 @@ getOrMakeModule mn = do
       modify_ \(RealState st) -> RealState st { modules = HM.insert mn emptyModule st.modules }
       pure emptyModule
 
-alterModule :: ModuleName -> ( (Module RealEval) -> Maybe (Module RealEval)) -> RealEval Unit
+alterModule :: ModuleName -> ((Module RealEval) -> Maybe (Module RealEval)) -> RealEval Unit
 alterModule mn f = modify_ \(RealState st) -> RealState st { modules = HM.alter (fromMaybe emptyModule >>> f) mn st.modules }
 
 getOrMakeStack :: ModuleName -> StackName -> RealEval RStack
 getOrMakeStack mn sn = do
   m@{ stacks } <- getOrMakeModule mn
-  let outStack = case HM.lookup sn stacks of
-        Just stack -> stack
-        Nothing -> emptyStack -- impossible
+  let
+    outStack = case HM.lookup sn stacks of
+      Just stack -> stack
+      Nothing -> emptyStack -- impossible
   alterModule mn (\_ -> pure $ m { stacks = HM.insert sn outStack stacks })
   pure outStack
 
 alterStack :: ModuleName -> StackName -> (RStack -> Maybe RStack) -> RealEval Unit
 alterStack mn sn f = alterModule mn \m@{ stacks } -> pure $ m { stacks = HM.alter (fromMaybe emptyStack >>> f) sn stacks }
 
---modify_ \(RealState st) -> RealState st { modules = HM.alter (fromMaybe emptyModule >>> f) mn st.modules }
-
+getOpenModule :: RealEval ModuleName
+getOpenModule = get <#> \(RealState { openModules }) -> NA.head openModules
 
 --| Evaluation monad for the langauge
 class (Monad m, MonadEffect m, MonadThrow String m) <= MonadEval m where
@@ -77,15 +92,21 @@ class (Monad m, MonadEffect m, MonadThrow String m) <= MonadEval m where
   nextWordRaw :: m (Maybe String)
   --| Add a definition, given a module name, definition name, and definition. 
   record :: ModuleName -> String -> Array String -> m Unit
-  --| Run a continuation in a module.
-  execute :: ModuleName -> Array String -> m Unit
+  recordNative :: ModuleName -> String -> m Unit -> m Unit
+  --| Switch the active module used for execution.
+  enter :: ModuleName -> m Unit
+  leave :: m Unit
+  --| Run a continuation in the current module, one word at a time.
+  execute :: WordName -> m Unit
   --| Add a dependency between modules for name resolution. Puts a module _inside_ another module
   depend :: ModuleName -> ModuleName -> m Unit
-  --| Push to a named stack in a module. Creates a stack if it doesn't exist.
+  --| Push to a named stack in the current module. Creates a stack if it doesn't exist.
   push :: ModuleName -> StackName -> RValue -> m Unit
-  pop :: ModuleName -> StackName -> m RValue
+  pop :: ModuleName -> StackName -> m (Maybe RValue)
   --| Peek at a value on a stack
   peek :: ModuleName -> StackName -> Int -> m RValue
+  --| Look up a name in a module chain
+  lookup :: ModuleName -> WordName -> m (Maybe (Definition m))
 
 instance monadEvalRealEval :: MonadEval RealEval where
   nextWordRaw :: RealEval (Maybe String)
@@ -94,28 +115,85 @@ instance monadEvalRealEval :: MonadEval RealEval where
     RealState { source, sourceIx } <- get
     pure $ A.index source sourceIx
   record :: ModuleName -> WordName -> Array WordName -> RealEval Unit
-  record mn n def = pure unit
+  record mn name def = alterModule mn \m -> pure $ m { defs = HM.insert name (Canon def) m.defs }
+  recordNative :: ModuleName -> WordName -> RealEval Unit -> RealEval Unit
+  recordNative mn name def = alterModule mn \m -> pure $ m { defs = HM.insert name (Native def) m.defs }
   depend :: ModuleName -> ModuleName -> RealEval Unit
-  depend mn mn' = alterModule mn \m -> pure $ m { chain = mn':m.chain }
+  depend mn mn' = alterModule mn \m -> pure $ m { chain = mn' : m.chain }
   push :: ModuleName -> StackName -> RValue -> RealEval Unit
-  push mn sn rv = do
+  push mn sn rv = alterStack mn sn (\stack -> pure $ rv : stack)
+  pop :: ModuleName -> StackName -> RealEval (Maybe RValue)
+  pop mn sn = do
     stack <- getOrMakeStack mn sn
-    alterModule mn \m -> pure $ m { stacks = HM.insert sn (rv:stack) m.stacks }
-  pop :: ModuleName -> StackName -> RealEval RValue
-  pop mn sn = pure "unit"
+    case A.uncons stack of
+      Nothing -> pure Nothing -- TODO: log exception?
+      Just { head: x, tail: xs } -> do
+        alterStack mn sn (const (Just xs))
+        pure $ Just x
   peek :: ModuleName -> StackName -> Int -> RealEval RValue
   peek mn sn depth = pure "ok"
-  execute :: ModuleName -> Array String -> RealEval Unit
-  execute mn cont = pure unit--do
+  lookup :: ModuleName -> WordName -> RealEval (Maybe (Definition RealEval))
+  lookup mn name = do
+    m@{ defs, chain } <- getOrMakeModule mn
+    case HM.lookup name defs of
+      Just def -> pure $ Just def
+      -- First layer name resolution (in the direct module) failed:
+      -- let's do a BFS of the inheritance tree to try to resolve the name in a parent context
+      -- Note that this method can diverge, there is no static guarantee at the moment that the dependency graph is acyclic.
+      Nothing -> rec chain
+    where
+    rec :: Array ModuleName -> RealEval (Maybe (Definition RealEval))
+    rec mns = case A.uncons mns of
+      Nothing -> pure Nothing
+      Just { head, tail } -> do
+        maybeDef <- lookup head name
+        case maybeDef of
+          Nothing -> rec tail
+          Just def -> pure $ Just def
+  enter :: ModuleName -> RealEval Unit
+  enter mn = modify_ \(RealState st) -> RealState st { openModules = NA.cons mn st.openModules }
+  leave :: RealEval Unit
+  leave = do
+    RealState { openModules } <- get
+    let { head: _, tail } = NA.uncons openModules
+        maybeNewModules = NA.fromArray tail -- fails if tail is empty
+    case maybeNewModules of 
+      Nothing -> pure unit -- tried to `leave` the final module, just no-op
+      Just newModules -> modify_ \(RealState st) -> RealState st { openModules = newModules }
+  execute :: WordName -> RealEval Unit
+  execute cont = do
+    mn <- getOpenModule
+    maybeDef <- lookup mn cont
+    case maybeDef of
+      Nothing -> throwError $ "unknown word " <> cont
+      -- TODO: also handle syntax words
+      Just (Native f) -> f
+      Just (Canon def) -> do
+        _ <- sequence $ map execute def
+        pure unit
 
---| Run a program's parse-time directives, like MODULE: 
---| Produce a new program for the second pass.
-firstPass :: forall m. MonadEval m => String -> m String
-firstPass = words >>> run
-  where
-    run :: Array String -> m String
-    run ws = pure "ok" 
+--| Execute the next word ready to be processed by the VM and seek forward.
+--| Follows two passes: if the word is a macro, it is executed immediately and may consume more words.
+--| If the word is a not a macro, execute its definition.
+executeNextWord :: forall m. MonadEval m => m Unit
+executeNextWord = do
+  maybeNw <- nextWordRaw
+  case maybeNw of
+    Nothing -> pure unit
+    Just nw -> execute nw
 
+evalRealState :: forall a. RealEval a -> RealState -> (Error -> Effect a) -> Effect a
+evalRealState action starting errHandler = let
+  throwable = evalStateT action starting
+  handled = catchError throwable errHandler
+  in handled
+
+--| Given a string that contains a program, evaluate the program entirely. 
+--| Log both error messages and output to stdout
+evaluate :: forall m. MonadEffect m => String -> m Unit
+evaluate str = let
+  startingState = RealState <<< _ { source = words str } $ unwrap emptyRealState
+  in evalRealState startingState executeNextWord
 
 words :: String -> Array String
 words = S.split (S.Pattern " ")
