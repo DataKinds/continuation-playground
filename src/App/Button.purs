b9036a836e242b1932c5aa7ae7dd4a8f77adcd7b
DataKinds <tslimkemann42@gmail.com>
2026-07-02 17:51:27 -0700

    Add raw input

--- src/App/Button.purs
+++ src/App/Button.purs
@@ -1,17 +1,35 @@
 module App.Button where
 
 import Prelude
+
+import Data.Maybe (fromJust)
+import Debug (traceM)
+import Effect.Aff.Class (class MonadAff)
+import Effect.Class (class MonadEffect)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
+import Partial.Unsafe (unsafePartial)
+import Safe.Coerce (coerce)
+import Web.DOM.Document as D
+import Web.DOM.Element (toNode)
+import Web.DOM.Node (appendChild)
+import Web.Event.Internal.Types (Event)
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (toDocument)
+import Web.HTML.Window (document)
+import Web.UIEvent.InputEvent as IE
+import Web.UIEvent.KeyboardEvent as KE
 
-type State
-  = { count :: Int }
+type State = { count :: Int }
 
 data Action
   = Increment
+  | RawInput IE.InputEvent
+  | RawKeyUp KE.KeyboardEvent
+  | RunCode String
 
-component :: forall q i o m. H.Component q i o m
+component :: forall q i o m. MonadEffect m => H.Component q i o m
 component =
   H.mkComponent
     { initialState: \_ -> { count: 0 }
@@ -27,8 +45,21 @@ render state =
     , HH.button
         [ HE.onClick \_ -> Increment ]
         [ HH.text "Click me" ]
+    , HH.input
+        [ HE.onInput (IE.fromEvent >>> unsafePartial fromJust >>> RawInput)
+        , HE.onKeyUp RawKeyUp
+         ]
     ]
 
-handleAction :: forall cs o m. Action → H.HalogenM State Action cs o m Unit
+handleAction :: forall cs o m. MonadEffect m => Action → H.HalogenM State Action cs o m Unit
 handleAction = case _ of
   Increment -> H.modify_ \st -> st { count = st.count + 1 }
+  RawInput ie -> pure unit
+  RawKeyUp ke -> traceM ke
+  RunCode code -> do
+    doc <- H.liftEffect (window >>= document)
+    elem <- H.liftEffect $ D.createElement "hello" (toDocument doc)
+    H.liftEffect $ appendChild (D.toNode $ toDocument doc) (toNode elem)
+    pure unit
+
+
